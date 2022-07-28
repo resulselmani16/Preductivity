1ce1e94b95d59fb7580175ca6d250eb97d5bae84
Resul Selmani <resul@frakton.com>
2022-07-28 02:31:08 +0200

    Add board page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
+import Board from "./pages/Board";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to={"/dashboard"} />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/board/:id" element={<Board />} />
         </Routes>
       </BrowserRouter>
     </div>
